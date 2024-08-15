282819fb52e60bdab995efca47416ddf0fde84dd
Shashi8969 <shashikumar10062@gmail.com>
2024-08-15 22:07:37 +0530

    add css for select

--- src/app/cart-page/cart-page.component.ts
+++ src/app/cart-page/cart-page.component.ts
@@ -12,11 +12,7 @@ import { food } from '../shared/models/food';
 })
 export class CartPageComponent implements OnInit{
   cart!: Cart;
-  constructor(private cartService: CartService, private foodService:FoodService){  
-    let foods = foodService.getAll();
-    cartService.addToCart(foods[1]);
-    cartService.addToCart(foods[3]);
-    cartService.addToCart(foods[5]);
+  constructor(private cartService: CartService){  
     this.setCart();
   }
 
