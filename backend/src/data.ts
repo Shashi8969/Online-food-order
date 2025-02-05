2986f7aed742ee488524efe7ab377f3afb603632
SHASHI KUMAR <81017570+Shashi8969@users.noreply.github.com>
2025-02-05 22:34:50 +0530

    Update data.ts

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -124,18 +124,5 @@ export const sample_tags: any[] = [
 ];
 
 export const sample_users: any[] = [
-  {
-    name: 'John Doe',
-    email: 'john@gmail.com',
-    password: '12345',
-    address: 'Toronto On',
-    isAdmin: true,
-  },
-  {
-    name: 'Jane Doe',
-    email: 'Jane@gmail.com',
-    password: '12345',
-    address: 'Shanghai',
-    isAdmin: false,
-  },
+  
 ];
